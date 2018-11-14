74d493ecf7c09c6e22f7d6a118fe23b26e899c1b
Dinesh Reddy Parne <dineshparne@gmail.com>
2018-11-14 14:35:51 -0600

    Added inquirer and axios to do postman api calls and present choices to user

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-import * as newman from 'newman';
-import * as readline from 'readline';
+import newman from 'newman';
+import readline from 'readline';
 import program from 'commander';
+import inquirer from 'inquirer';
+import axios from 'axios';
+import fs from 'fs';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -19,9 +22,55 @@ const askQuestion = (question: string) => {
 };
 
 program
-    .version('0.1.0')
-    .action(() => {
-        console.log('hello');
-        process.exit(0);
+    .version('0.0.1')
+    .action(async () => {
+        try {
+            const postmanConfig: any = {
+                apiKey: process.env.POSTMAN_API_KEY
+            };
+            if (!process.env.POSTMAN_API_KEY) {
+                postmanConfig.apiKey = await askQuestion('please enter your postman api key -> ');
+            }
+            let collectionsResponse: any = await axios.get('https://api.getpostman.com/collections', {
+                headers: {
+                    'X-Api-Key': postmanConfig.apiKey
+                }
+            });
+            let choices = collectionsResponse.data.collections.map((choice: any) => {
+                return { name: choice.name, value: choice.uid }
+            })
+            console.log(choices);
+            let collectionAnswer = await inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'collection',
+                        message: 'Pick the collection -> ',
+                        choices: choices
+                    }
+                ]);
+            let environmentsResponse: any = await axios.get('https://api.getpostman.com/environments', {
+                headers: {
+                    'X-Api-Key': postmanConfig.apiKey
+                }
+            });
+            let environments = environmentsResponse.data.environments.map((environment: any) => {
+                return { name: environment.name, value: environment.uid }
+            })
+            console.log(environments);
+            let environmentAnswer = await inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'environment',
+                        message: 'Pick the environment -> ',
+                        choices: environments
+                    }
+                ]);
+            process.exit(0);
+        } catch (error) {
+            console.log(error);
+            process.exit(1);
+        }
     })
     .parse(process.argv);
