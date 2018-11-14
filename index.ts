59b33ee996b0f6b72bea9948187497577184a0cc
Dinesh Reddy Parne <dineshparne@gmail.com>
2018-11-14 11:30:26 -0600

    Added readline module and also bumped target to es6

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
 import * as newman from 'newman';
+import * as readline from 'readline';
 
-console.log('Hello!');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const askQuestion = (question: string) => {
+    return new Promise<string>((resolve) => {
+        rl.question(question, (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+};
+
+askQuestion('please enter your postman api key -> ');
